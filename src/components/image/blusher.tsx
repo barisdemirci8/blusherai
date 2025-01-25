bba45cfbb7d0b02a09357c99192f976bd0580a49
Baris Demirci <baris8@Bariss-MBP.sunrise.box>
2025-01-25 15:06:43 +0100

    fix shadcn theme not applied to comps, brush and clear draw done

--- src/components/image/blusher.tsx
+++ src/components/image/blusher.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { useRef, useState } from "react";
+import { Button } from "../ui/button";
+import { Slider } from "../ui/slider";
 
 export default function Blusher() {
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     const [isDrawing, setIsDrawing] = useState<boolean>(false);
+    const [brushSize, setBrushSize] = useState<number>(20);
 
     const startDraw = (e: React.MouseEvent<HTMLCanvasElement>) => {
         console.log('starting draw: ', e);
@@ -42,37 +45,59 @@ export default function Blusher() {
             console.log('y: ', y);
 
             ctx.beginPath();
-            ctx.arc(x, y, 50, 0, 2 * Math.PI);
+            ctx.arc(x, y, brushSize / 2, 0, 2 * Math.PI);
             ctx.stroke();
         }
 
         //console.log('draw: ', e);
     }
+
+    const clearDrawing = () => {
+
+        const canvas = canvasRef.current;
+        const ctx = canvas?.getContext("2d");
+
+        if (canvas && ctx) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
+    }
+
+    const handleBrushChange = (values: number[]) => {
+        setBrushSize(values[0]);
+    }
     
-    return(
-        <div className="flex justify-center items-center cursor-pointer">
-            
-           {/* <div className="w-128 h-128">
+    return (
+      <div className="flex flex-col justify-center items-center cursor-pointer gap-3">
+        {/* <div className="w-128 h-128">
                 <img 
                     src={"/images/ducks.png"}
                     alt="test image"
                     className="object-fill rounded-sm"
                 />
             </div>*/}
-            
-            <canvas 
-                ref={canvasRef}
-                className="border-2 border-red-200"
-                width={500}
-                height={500}
-                onMouseDown={startDraw}
-                onMouseUp={stopDraw}
-                onMouseLeave={stopDraw}
-                onMouseMove={draw}
-            />
-            
-            
-
+        <div className="flex justify-center items-center gap-3 w-full">
+          <Slider
+            value={[brushSize]}
+            onValueChange={handleBrushChange}
+            step={1}
+            max={100}
+            min={1}
+          />
+          <Button onClick={clearDrawing}>
+            Reset
+          </Button>
         </div>
+
+        <canvas
+          ref={canvasRef}
+          className="border-2 border-red-200"
+          width={500}
+          height={500}
+          onMouseDown={startDraw}
+          onMouseUp={stopDraw}
+          onMouseLeave={stopDraw}
+          onMouseMove={draw}
+        />
+      </div>
     );
 }
