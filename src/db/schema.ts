7bf856331ed990998d8da7d04ecf0ce220a9b142
Baris Demirci <baris.demirci93@hotmail.com>
2025-03-18 21:45:50 +0100

    user schema naming

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,11 +7,11 @@ import {
   integer,
 } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
+export const usersTable = pgTable("users", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
   email: varchar("email", { length: 255 }).notNull().unique(),
   password: varchar("password").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
-export type TUser = typeof users.$inferSelect;
+export type TUser = typeof usersTable.$inferSelect;
