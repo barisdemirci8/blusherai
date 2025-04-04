0444e3125434b4b5efec93ce2d6f401782fe9f50
Baris Demirci <baris.demirci93@hotmail.com>
2025-04-04 16:46:21 +0200

    remove test file, fix layouting landing page mobile and desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,41 @@ import BlusherForm from "@/components/image/blusherForm";
 export default function Home() {
   return (
     <main className="flex flex-col justify-start align-center lg:p-12">
-      <section className="lg:p-8 lg:max-w-1/2 bg-red-200">
-        <div className="flex gap-2 text-4xl font-bold text-muted-foreground overflow-hidden whitespace-nowrap">
-          <AnimatedKeyword />
-          <h1 className="inline-block">your image</h1>
+      <section className="grid grid-rows-4 grid-cols-1 md:grid-cols-2">
+        <div className="p-4 lg:p-8">
+          <div className="flex gap-2 text-4xl font-bold text-muted-foreground overflow-hidden whitespace-nowrap">
+            <AnimatedKeyword />
+            <h1 className="inline-block">your image</h1>
+          </div>
+          <p className="text-xl bg-gradient-to-r from-[#24699d] to-[#791656] bg-clip-text text-transparent">
+            Upload your image, blur parts of it that you don&apos;t want and let
+            this &quot;fancy Chat-GPT wrapper&quot; generate a replacment based
+            on your input
+          </p>
         </div>
-        <p className="text-xl bg-gradient-to-r from-[#24699d] to-[#791656] bg-clip-text text-transparent">
-          Upload your image, blur parts of it that you don&apos;t want and let
-          this &quot;fancy Chat-GPT wrapper&quot; generate a replacment based on
-          your input
-        </p>
+        <div className="md:row-span-2 md:col-start-2 p-2 self-end">
+          <img
+            alt=""
+            src="/images/bg_1_copy.png"
+            className="w-2/3 h-auto object-fill rounded-xl"
+          />
+        </div>
+        <div className="md:row-span-2 self-right p-2 justify-items-end self-center">
+          <img
+            alt=""
+            src="/images/bg_1_copy.png"
+            className="w-2/3 h-auto object-fill rounded-xl"
+          />
+        </div>
+        <div className="md:row-span-2 p-2">
+          <img
+            alt=""
+            src="/images/bg_1_copy.png"
+            className="w-2/3 h-auto object-fill rounded-xl"
+          />
+        </div>
       </section>
-      {/* <BlusherForm /> */}
+      <BlusherForm />
     </main>
   );
 }
